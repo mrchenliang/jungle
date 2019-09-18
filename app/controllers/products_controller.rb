e9a8f6aba0c40068f4e0d6447a285dd2c59f7161
mrchenliang <c.liang@ualberta.ca>
2019-09-18 10:58:10 -0400

    adding http basic auth for user login/password user:Chen password:password

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-
+  http_basic_authenticate_with name: ENV['STRIPE_ADMIN_USERNAME'], password: ENV['STRIPE_ADMIN_PASSWORD']
   def index
     @products = Product.all.order(created_at: :desc)
   end
