bc3f7642b0f0f9762a91d7eceae8864fb16eeb53
mrchenliang <c.liang@ualberta.ca>
2019-09-19 15:15:43 -0400

    creating user session by verifying password and username

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  resources :about, only: [:index]
   resources :products, only: [:index, :show]
   resources :categories, only: [:show, :update, :show]
-  resources :about, only: [:index]
+
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 
   get '/about' , to: 'about#show'
 
--- db/schema.rb
+++ db/schema.rb
@@ -65,9 +65,9 @@ ActiveRecord::Schema.define(version: 20190919181002) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   add_foreign_key "line_items", "orders"
