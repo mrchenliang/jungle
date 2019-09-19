80411dfcdc0334fc85408fce18d2dff39b64b390
mrchenliang <c.liang@ualberta.ca>
2019-09-19 16:01:36 -0400

    fixing create user error

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['STRIPE_ADMIN_USERNAME'], password: ENV['STRIPE_ADMIN_PASSWORD']
   def show
     @product_count = Product.count
     @category_count = Category.count
