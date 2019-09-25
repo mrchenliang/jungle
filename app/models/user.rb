58e86ec79a30090c40253df68319d93ad43f71d4
mrchenliang <c.liang@ualberta.ca>
2019-09-25 18:36:47 -0400

    fixing login/signup errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 5}
 
   def self.authenticate_with_credentials(email, password)
-    email = email.downcase.strip
+    email = email
     @user = User.find_by_email(email)
     if @user && @user.authenticate(password)
       @user
